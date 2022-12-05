ab951785cc8a93aae4f5780a3b2efa6be3f3cf79
Sevindzh Shukurova <shukurova.s.n@gmail.com>
2022-12-05 00:26:38 -0500

    PreEntrega2 Shukurova Sevindzh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Component } from "react";
 export class Footer extends Component {
     render () {
         return (
-            <span>Derechos reservados por autor</span>
+            <span>Derechos reservados por autor!</span>
         )
     }
 }
